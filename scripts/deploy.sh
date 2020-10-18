a4d545307ecfc2d33e49b2a3cdd67b7f6da78e06
Aditya Rahman <adityarahman032@gmail.com>
2020-10-18 20:59:51 +0800

    Update CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -x
-rsync -avP --exclude-from=exclude ./ ./docusaurus/Wiki
-rsync -avP ./Assets/ ./docusaurus/static/Assets
+rsync -avP --exclude-from=exclude --delete-after ./ ./docusaurus/Wiki
+rsync -avP --delete-after ./Assets/ ./docusaurus/static/Assets
 cd docusaurus
 yarn install
 yarn deploy
