a413c1675d038d0a7bc9e31c7837b3052dfeee19
Samsul Maarif <hay@samsul.web.id>
2020-10-28 16:17:55 +0700

    [samsul] add script to move attachment
    Signed-off-by: Samsul Maarif <hay@samsul.web.id>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 set -x
 rsync -aP --exclude-from=exclude --delete-during ./ ./docusaurus/Wiki
 rsync -aP --delete-during ./Assets/ ./docusaurus/static/Assets
+rsync -aP --delete-during ./raw-attachment ./docusaurus/static/
 cd docusaurus
 yarn install
 yarn deploy
