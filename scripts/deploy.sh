da8e735df0254a4d0418303be90c0f671ca395a4
Aditya Rahman <adityarahman032@gmail.com>
2020-11-25 23:07:07 +0800

    Update actions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-rsync -aP --exclude-from=exclude --delete-during ./ ./docusaurus/Wiki
-rsync -aP --delete-during ./Assets/ ./docusaurus/static/Assets
-rsync -aP --delete-during ./raw-attachment ./docusaurus/static/
+rsync -a --exclude-from=exclude --delete-during ./ ./docusaurus/Wiki
+rsync -a --delete-during ./Assets/ ./docusaurus/static/Assets
+rsync -a --delete-during ./raw-attachment ./docusaurus/static/
 
 pushd docusaurus
-yarn install
+yarn
 yarn deploy
 popd
